19f1de2d74b15805206a106450b951bea4e374f7
Michael Eisenbraun <eisenbm@algonquincollege.com>
2018-05-17 07:40:02 -0400

    Adds pseudocode to hangman.js

--- hangman.js
+++ hangman.js
@@ -1,3 +1,49 @@
 /**
  * Hangman
  */
+
+/**
+ * Create a list of phrases. Each phrase should have a related category.
+ */
+
+/**
+ * Create a container to hold the current game status
+ * - An active game indicator
+ * - The current phrase and category
+ * - A list of guessed letters
+ * - The number of strikes
+ * - The maximum number of allowed strikes
+ */
+
+/**
+ * Create and display a list of instructions to explain how to play the game
+ * and which commands can be used.
+ */
+
+/**
+ * Create a start command to start a new game
+ * - Set the active game indicator to true
+ * - Select a current phrase
+ * - Reset the number of strikes
+ * - Reset the list of guessed letters
+ * - Convert phrase to a puzzle
+ * - Display the puzzle with the current category
+ */
+
+/**
+ * Convert phrase to a puzzle
+ * -
+ */
+
+/**
+ * Create a guess command to guess a letter
+ * - Check if guessed letter is a valid letter
+ * - Check if guessed letter has not already been guessed
+ * - Add guessed letter to list of guessed letters
+ * - Check if guessed letter is in the phrase
+ *    - if yes, display a message that the guess was successful
+ *    - if no, update number of strikes
+ *      display message tell the player how many strikes are remaining
+ * - Convert the phrase to a puzzle with any any correctly guessed letters
+ * - Display the puzzle
+ */
